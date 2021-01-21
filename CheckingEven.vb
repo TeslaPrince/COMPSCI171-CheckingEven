1436c97230dbc2a8c1de6e8cecc032ade96088a0
Jenna Lambert <57016635+TeslaPrince@users.noreply.github.com>
2021-01-21 01:54:01 -0600

    Add files via upload

---
+++ CheckingEven.vb
@@ -0,0 +1,15 @@
+﻿Public Class CheckingEven
+    'CheckingEven program
+    'This program checks whether the input integer 
+    'is an even or not.
+
+    Private Sub checkButton_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
+        Dim userIn As Integer
+        Dim result As Boolean
+        userIn = CInt(txtInput.Text)
+        'check is userIn is an even
+        'if the number is divisible by 2
+        result = (userIn Mod 2 = 0)
+        lblDisplay.Text = CStr(result)
+    End Sub
+End Class
